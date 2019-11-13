32d09e1dd630f4e14b385fd6a790b75dfa8d6e9d
Marija Jelicic <marija.jelicic@netcast.rs>
2019-11-13 17:18:42 +0100

    -- eureka client

--- src/main/java/rs/raf/library/LibraryApplication.java
+++ src/main/java/rs/raf/library/LibraryApplication.java
@@ -3,10 +3,12 @@ package rs.raf.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class LibraryApplication {
 
 	public static void main(String[] args) {
--- src/main/java/rs/raf/library/service/feign/QuoteFeignClient.java
+++ src/main/java/rs/raf/library/service/feign/QuoteFeignClient.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import rs.raf.library.domain.dto.QuoteDto;
 
-@FeignClient(name = "quotes", url = "http://localhost:8081/quotes")
+@FeignClient(name = "quotes", url = "http://localhost:8080/quotes")
+
 public interface QuoteFeignClient {
 
-	@RequestMapping(value = "/getQuote", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/quotes/getQuote", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody QuoteDto getQuote();
 }
